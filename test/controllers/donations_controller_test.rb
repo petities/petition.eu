4440638a9c2df906745854011b2b141722c67625
Simon Koolstra <skoolstra@users.noreply.github.com>
2025-01-09 17:31:01 -0500

    Also update location in tests

--- test/controllers/donations_controller_test.rb
+++ test/controllers/donations_controller_test.rb
@@ -4,6 +4,6 @@ class DonationsControllerTest < ActionController::TestCase
   test 'should redirect index to payment request' do
     get :index
     assert_response :redirect
-    assert_redirected_to 'https://betaalverzoek.rabobank.nl/betaalverzoek/?id=MX3AG14USwamtfVmuwZxng'
+    assert_redirected_to 'https://betaalverzoek.rabobank.nl/betaalverzoek/?id=85HKE9N9RI-yMJtHcc5MqA'
   end
 end
